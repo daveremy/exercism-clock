6cb090d4690561a0afa17b162ed96c87f2869fd2
Dave Remy <email@daveremy.com>
2019-11-04 13:00:20 +0100

    Changes based on mentor recommendation

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,41 @@
 use std::fmt;
 
-#[derive(Debug, PartialEq, Clone)]
-struct Hours(i32);
-
-impl Hours {
-    fn new(hours: i32) -> Hours {
-        let hours = hours % 24;
-        if hours < 0 {
-            Hours(24 + hours)
-        } else {
-            Hours(hours)
-        }
-    }
-}
-
-impl fmt::Display for Hours {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", to_fixed_2_char_string(self.0.to_string()))
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-struct Minutes(i32);
-
-impl Minutes {
-    fn new(minutes: i32) -> (Minutes, i32) {
-        let hours_adjustment = minutes / 60;
-        let minutes = minutes % 60;
-        if minutes < 0 {
-            (Minutes(60 + minutes), hours_adjustment - 1)
-        } else {
-            (Minutes(minutes), hours_adjustment)
-        }
-    }
-}
-
-impl fmt::Display for Minutes {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", to_fixed_2_char_string(self.0.to_string()))
-    }
-}
-
-fn to_fixed_2_char_string(s: String) -> String {
-    match s.len() {
-        2 => s,
-        1 => format!("0{}", s),
-        _ => panic!("String must be 1 or 2 chars"),
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug)]
 pub struct Clock {
-    hours: Hours,
-    minutes: Minutes,
+    minutes: i32,
 }
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        let (minutes, hours_adjustment) = Minutes::new(minutes);
-        let hours = Hours::new(hours + hours_adjustment);
-        Clock { hours, minutes }
-    }
-
-    pub fn add_hours(&self, hours: i32) -> Self {
+    pub fn new(hours: i32, minutes: i32) -> Clock {
         Clock {
-            hours: Hours(self.hours.0 + hours),
-            minutes: self.minutes.clone(),
+            minutes: (hours * 60) + minutes,
         }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        let (new_minutes, hours_adjustment) = Minutes::new(minutes + self.minutes.0);
-        let new_hours = Hours::new(self.hours.0 + hours_adjustment);
+    pub fn add_minutes(&self, minutes: i32) -> Clock {
         Clock {
-            hours: new_hours,
-            minutes: new_minutes,
+            minutes: self.minutes + minutes,
         }
+    }
+
+    fn get_hours_minutes(&self) -> (u32, u32) {
+        (
+            self.minutes.div_euclid(60).rem_euclid(24) as u32,
+            self.minutes.rem_euclid(60) as u32,
+        )
     }
 }
 
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.get_hours_minutes() == other.get_hours_minutes()
+    }
+}
+impl Eq for Clock {}
+
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}:{}", self.hours.to_string(), self.minutes.to_string())
+        let (hours, minutes) = self.get_hours_minutes();
+        write!(f, "{:0width$}:{:0width$}", hours, minutes, width = 2)
     }
 }
